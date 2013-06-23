8f3dca2b73ebc54be15e39e9fbddcb4c7cb8a6ba
Inderpal Singh <inderps@thoughtworks.com>
2013-06-24 00:23:25 +0530

    now create api update visitor if already present

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
-
+gem 'pry'
 gem 'pg'
 
 
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,11 +1,26 @@
 class VisitorsController < ApplicationController
   def create
-    visitor = Visitor.create(identifier: params[:identifier], visit_count: 0)
+    visitor = Visitor.find_by_identifier(params[:identifier])
+    if visitor.present?
+      visitor.visit_count = visitor.visit_count + 1
+      visitor.save
+    else
+      visitor = Visitor.create(identifier: params[:identifier], visit_count: 1)
+    end
+
     render json: { visitor: visitor }
   end
 
   def index
     visitors = Visitor.all
     render json: { visitors: visitors }
+  end
+
+  def signup
+    visitor = Visitor.find_by_identifier(identifier: params[:identifier])
+    visitor.email = params[:email]
+    visitor.visit_time = Time.now
+    visitor.save
+
   end
 end
