3173a3ee37f5566a8951047c46d9a7c246511a11
Inderpal Singh <inderps@thoughtworks.com>
2013-06-23 17:27:21 +0530

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuotationSignupService::Application.config.secret_token = '37a9795908aea51affa43d06a26f16b9a7ab9f65ef1c26ca6f9c92665fd5705cb339ed7355cf3761c652c5adbaf5c2d31e906cfd93aa83ed7633cd43509f2310'
